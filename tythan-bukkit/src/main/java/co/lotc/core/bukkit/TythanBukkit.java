d7a466b296694a128daefc4925ad97aceed8327d
Sporadic <pjcbakker1@gmail.com>
2019-01-16 10:06:38 +0100

    serialize weakblock

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/TythanBukkit.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/TythanBukkit.java
@@ -1,11 +1,13 @@
 package co.lotc.core.bukkit;
 
 import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import co.lotc.core.Tythan;
 import co.lotc.core.TythanCommon;
 import co.lotc.core.bukkit.util.ChatBuilder;
+import co.lotc.core.bukkit.util.WeakBlock;
 import lombok.Getter;
 
 public class TythanBukkit extends JavaPlugin implements Tythan {
@@ -15,6 +17,7 @@ public class TythanBukkit extends JavaPlugin implements Tythan {
 	@Override
 	public void onLoad() {
 		common.onLoad();
+		ConfigurationSerialization.registerClass(WeakBlock.class, "WeakBlock");
 	}
 	
 	@Override
