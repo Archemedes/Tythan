a38e9e1beaba5c468bd192e5baed8165a0833a8b
Sporadic <pjcbakker1@gmail.com>
2019-01-23 15:20:42 +0100

    context object

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/menu/MenuAgent.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/menu/MenuAgent.java
@@ -1,62 +1,47 @@
 package co.lotc.core.bukkit.menu;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.lang.Validate;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
-import co.lotc.core.Tythan;
 import co.lotc.core.bukkit.TythanBukkit;
 import co.lotc.core.bukkit.util.Run;
+import co.lotc.core.util.Context;
 import lombok.Getter;
 
 public class MenuAgent {
 	@Getter private final Player player;
 	@Getter private	Menu menu = null; //Current menu focused on, may change
 	
-	private final Map<String, Object> context = new HashMap<>();
+	private final Context context = new Context();
 	
 	MenuAgent(Player player) {
 		this.player = player;
-	}
-	
-	void setInitialMenu(Menu menu) {
-		Validate.isTrue(this.menu == null, "Must be initiated by the Native MenuListener!");
-		
-		this.menu = menu;
 	}
 	
 	public Inventory getInventory() {
 		return menu.getInventory();
 	}
 	
-	public void openNewInventory(Menu newMenu) {
-		Run.as(getPlugin()).sync(()->{
+	public void open(Menu newMenu) {
+		Run.as(TythanBukkit.get()).sync(()->{
 			menu = newMenu;
 			player.getPlayer().openInventory(menu.getInventory());
 		});
 	}
 	
 	public boolean hasContext(String tag) {
-		return context.containsKey(tag);
+		return context.has(tag);
 	}
 	
 	public void addContext(String tag, Object value) {
-		context.put(tag, value);
+		context.set(tag, value);
 	}
 	
-	@SuppressWarnings("unchecked")
 	public <T> T getContext(String tag) {
-		return (T) context.get(tag);
+		return context.get(tag);
 	}
 	
 	public String getContextString(String tag) {
-		return (String) context.get(tag);
-	}
-
-	private TythanBukkit getPlugin() {
-		return (TythanBukkit) Tythan.get();
+		return context.getString(tag);
 	}
 }
---
+++ tythan-common/src/main/java/co/lotc/core/util/Context.java
@@ -0,0 +1,35 @@
+package co.lotc.core.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import lombok.Getter;
+
+public class Context implements Iterable<Entry<String, Object>> {
+	@Getter private final Map<String, Object> map = new HashMap<>();
+
+	public boolean has(String key) {
+		return map.containsKey(key);
+	}
+	
+	public void set(String key, Object value) {
+		map.put(key, value);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T get(String key) {
+		return (T) map.get(key);
+	}
+	
+	public String getString(String key) {
+		return String.valueOf(map.get(key));
+	}
+	
+	@Override
+	public Iterator<Entry<String, Object>> iterator() {
+		return map.entrySet().iterator();
+	}
+	
+}
