f46a46f04c5660f622bac2ba2a01024ca107d93c
Sporadic <pjcbakker1@gmail.com>
2019-01-28 21:45:53 +0100

    pls compile

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/ChatStream.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/ChatStream.java
@@ -4,16 +4,11 @@ import org.bukkit.entity.Player;
 
 import co.lotc.core.agnostic.AbstractChatStream;
 import co.lotc.core.bukkit.wrapper.BukkitSender;
-import net.md_5.bungee.api.chat.BaseComponent;
 
 public class ChatStream extends AbstractChatStream{
 
 	public ChatStream(Player p) {
 		super(new BukkitSender(p));
-	}
-	
-	public ChatStream prompt(String contextTag, BaseComponent text) {
-		this.prompt(contextTag, promptText, fulfillment)
 	}
 
 }
