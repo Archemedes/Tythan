1fa280749fb42974121a131405a6474fe0610b93
Sporadic <pjcbakker1@gmail.com>
2019-01-30 20:05:30 +0100

    some folder helpers

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/menu/MenuBuilder.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/menu/MenuBuilder.java
@@ -1,5 +1,6 @@
 package co.lotc.core.bukkit.menu;
 
+import static org.bukkit.ChatColor.WHITE;
 import static org.bukkit.Material.AIR;
 
 import java.util.Arrays;
@@ -10,10 +11,16 @@ import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 
 import co.lotc.core.bukkit.menu.icon.Icon;
+import co.lotc.core.bukkit.menu.icon.Link;
 import co.lotc.core.bukkit.menu.icon.Pad;
 import co.lotc.core.bukkit.menu.icon.SimpleIcon;
+import co.lotc.core.bukkit.util.ItemUtil;
 
 public class MenuBuilder {
 	final String title;
@@ -72,6 +79,34 @@ public class MenuBuilder {
 	
 	public MenuBuilder pad(int i, ItemStack is) {
 		icons.set(i, new Pad(is));
+		return this;
+	}
+	
+	public MenuBuilder folder(String name, Menu to) {
+		return folder(firstEmpty(), name, to);
+	}
+	
+	public MenuBuilder folder(int i, String name, Menu to) {
+		return folder(i, name, Suppliers.ofInstance(to), false);
+	}
+	
+	public MenuBuilder folder(String name, Supplier<Menu> to) {
+		return folder(firstEmpty(), name, to);
+	}
+	
+	public MenuBuilder folder(int i, String name, Supplier<Menu> to) {
+		return folder(i, name, to, true);
+	}
+	
+	public MenuBuilder folder(int i, String name, Supplier<Menu> to, boolean memoize) {
+		ItemStack is = ItemUtil.getSkullFromTexture("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTYzMzBhNGEyMmZmNTU4NzFmYzhjNjE4ZTQyMWEzNzczM2FjMWRjYWI5YzhlMWE0YmI3M2FlNjQ1YTRhNGUifX19");
+		ItemMeta m = is.getItemMeta();
+		m.setDisplayName(WHITE + name);
+		is.setItemMeta(m);
+		
+		Icon icon = memoize? Link.memoize(is, to) : new Link(is, to);
+		
+		icon(i, icon);
 		return this;
 	}
 	
