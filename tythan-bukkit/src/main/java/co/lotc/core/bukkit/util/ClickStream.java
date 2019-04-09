0116613ef542533e522087c6260e0a39c055740e
Sporadic <pino.bakker@gmail.com>
2019-04-09 20:15:29 +0200

    proposition for clickstream util class
    lets players select blocks or entities through right click

---
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/ClickStream.java
@@ -0,0 +1,35 @@
+package co.lotc.core.bukkit.util;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public final class ClickStream {
+
+	private ClickStream() {}
+	
+	
+	public static void selectBlock(Player p, Consumer<Block> callback) {
+		selectBlock(p, callback, b->true);
+	}
+	
+	public static void selectBlock(Player p, Consumer<Block> callback, Predicate<Block> filter) {
+		//TODO
+	}
+	
+	public static void selectPlayer(Player p, Consumer<Player> callback) {
+		selectEntity(p, e->callback.accept((Player) e), Player.class::isInstance);
+	}
+	
+	public static void selectEntity(Player p, Consumer<Entity> callback) {
+		selectEntity(p, callback, e->true);
+	}
+	
+	public static void selectEntity(Player p, Consumer<Entity> callback, Predicate<Entity> filter) {
+		//TODO
+	}
+
+}
