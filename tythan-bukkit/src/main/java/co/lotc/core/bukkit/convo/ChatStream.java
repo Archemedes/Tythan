543b747120a57026c934df9d5c425daa305c19de
Sporadic <pino.bakker@gmail.com>
2019-04-09 21:22:40 +0200

    chatstream clickeroo magic

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/convo/ChatStream.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/convo/ChatStream.java
@@ -2,13 +2,18 @@ package co.lotc.core.bukkit.convo;
 
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.google.common.base.Functions;
-import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
 import co.lotc.core.agnostic.AbstractChatStream;
@@ -52,10 +57,52 @@ public class ChatStream extends AbstractChatStream<ChatStream>{
 	public ChatStream prompt(String contextTag, BaseComponent message, Predicate<String> filter, Function<String, ?> mapper) {
 		return listen(contextTag, message, AsyncPlayerChatEvent.class, x->{
 			if(x.getPlayer().getUniqueId().equals(uuid)) {
-				if(filter.apply(x.getMessage())) return mapper.apply(x.getMessage());
+				if(filter.test(x.getMessage())) return mapper.apply(x.getMessage());
 			}
 			
 			return null;
+		});
+	}
+	
+	public ChatStream clickBlockPrompt(String contextTag, String message) {
+		return clickBlockPrompt(contextTag, new TextComponent(message));
+	}
+	
+	public ChatStream clickBlockPrompt(String contextTag, String message, Predicate<Block> filter) {
+		return clickBlockPrompt(contextTag, new TextComponent(message), filter);
+	}
+	
+	public ChatStream clickBlockPrompt(String contextTag, BaseComponent message) {
+		return clickBlockPrompt(contextTag, message, Predicates.alwaysTrue());
+	}
+	
+	public ChatStream clickBlockPrompt(String contextTag, BaseComponent message, Predicate<Block> filter) {
+		return listen(contextTag, message, PlayerInteractEvent.class, e->{
+				if(e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+					Block b = e.getClickedBlock();
+					if(filter.test(b)) return b;
+				}
+				return null;
+		});
+	}
+	
+	public ChatStream clickEntityPrompt(String contextTag, String message) {
+		return clickEntityPrompt(contextTag, new TextComponent(message));
+	}
+	
+	public ChatStream clickEntityPrompt(String contextTag, String message, Predicate<Entity> filter) {
+		return clickEntityPrompt(contextTag, new TextComponent(message), filter);
+	}
+	
+	public ChatStream clickEntityPrompt(String contextTag, BaseComponent message) {
+		return clickEntityPrompt(contextTag, message, Predicates.alwaysTrue());
+	}
+	
+	public ChatStream clickEntityPrompt(String contextTag, BaseComponent message, Predicate<Entity> filter) {
+		return listen(contextTag, message, PlayerInteractEntityEvent.class, e->{
+				Entity entity = e.getRightClicked();
+				if(filter.test(entity)) return entity;
+				return null;
 		});
 	}
 	
