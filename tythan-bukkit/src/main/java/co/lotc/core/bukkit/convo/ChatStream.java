3e9c17006502196388314a5bf15fe79be4fbf24f
Sporadic <pino.bakker@gmail.com>
2019-04-09 21:09:08 +0200

    move the promptlistener: will use it for ClickStream as well

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/convo/ChatStream.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/convo/ChatStream.java
@@ -1,33 +1,18 @@
 package co.lotc.core.bukkit.convo;
 
 import java.util.UUID;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
-import org.bukkit.event.EventException;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.EventExecutor;
-import org.bukkit.scheduler.BukkitTask;
 
 import com.google.common.base.Functions;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
-import co.lotc.core.CoreLog;
 import co.lotc.core.agnostic.AbstractChatStream;
-import co.lotc.core.bukkit.TythanBukkit;
-import co.lotc.core.bukkit.util.Run;
 import co.lotc.core.bukkit.wrapper.BukkitSender;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.var;
-import lombok.experimental.FieldDefaults;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 
@@ -40,7 +25,7 @@ public class ChatStream extends AbstractChatStream<ChatStream>{
 	}
 
 	public <T extends Event> ChatStream listen(String contextTag, BaseComponent message, Class<T> c, Function<T, Object> listener) {
-		prompt(contextTag, message, new HeyListen<>(uuid, c, listener));
+		prompt(contextTag, message, new PromptListener<>(uuid, c, listener));
 		return this;
 	}
 	
@@ -77,74 +62,5 @@ public class ChatStream extends AbstractChatStream<ChatStream>{
 	@Override
 	protected ChatStream getThis() {
 		return this;
-	}
-	
-	//Don't know if this will work
-	@FieldDefaults(level=AccessLevel.PRIVATE)
-	@RequiredArgsConstructor
-	private static class HeyListen<T extends Event> implements Consumer<Prompt>,Listener,EventExecutor {
-		final UUID uuid;
-		final Class<T> clazz;
-		final Function<T, Object> function;
-		
-		Prompt prompt;
-		BukkitTask giveUp;
-		
-		@Override
-		public void accept(Prompt p) {
-			prompt = p;
-			task();
-			var tythan = TythanBukkit.get();
-			var m = Bukkit.getPluginManager();
-			
-			m.registerEvent(AsyncPlayerChatEvent.class, this, EventPriority.LOW, this, tythan, true);
-			if(clazz != AsyncPlayerChatEvent.class) m.registerEvent(clazz, this, EventPriority.LOW, this, tythan, true);
-		}
-
-		@Override
-		public void execute(Listener listener, Event event) throws EventException {
-			CoreLog.debug("Catching Event as " + this);
-			
-			if(event instanceof AsyncPlayerChatEvent) {
-				var as = (AsyncPlayerChatEvent) event;
-				as.setCancelled(true);
-				if("stop".equals(as.getMessage().toLowerCase()) && as.getPlayer().getUniqueId().equals(uuid) ) {
-					Run.as(TythanBukkit.get()).sync(()->abandon());
-					return;
-				}
-			}
-			
-			if(clazz.isInstance(event)) {
-				task();
-				T theEvent = clazz.cast(event);
-				Object result = function.apply(theEvent);
-				if(result != null) {
-					abandon();
-					prompt.fulfil(result);
-					CoreLog.debug("Prompt was fulfilled: " + this);
-				} else {
-					prompt.sendPrompt();
-				}
-			}
-		}
-		
-		private void task() {
-			if(giveUp != null) giveUp.cancel();
-			giveUp = Bukkit.getScheduler().runTaskLater(TythanBukkit.get(), ()->{
-				giveUp = null;
-				abandon();
-				CoreLog.debug("Giving up on prompt " + this);
-			}, 20*20);
-		}
-		
-		private void abandon() {
-			HandlerList.unregisterAll(this);
-			if(giveUp != null) giveUp.cancel();
-		}
-		
-		@Override
-		public String toString() {
-			return "HeyListen{" + uuid.toString() + "->" + prompt.getText().toLegacyText() + "}";
-		}
 	}
 }
