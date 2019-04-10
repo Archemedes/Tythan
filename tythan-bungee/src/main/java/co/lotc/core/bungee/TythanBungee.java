671504821bb0d4cbb60ae381e63aa8c963445646
George Lin <17georgel@gmail.com>
2019-04-10 21:05:39 +0800

    Register ProxiedPlayer command parameter

--- tythan-bungee/src/main/java/co/lotc/core/bungee/TythanBungee.java
+++ tythan-bungee/src/main/java/co/lotc/core/bungee/TythanBungee.java
@@ -1,13 +1,9 @@
 package co.lotc.core.bungee;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-
 import co.lotc.core.Tythan;
 import co.lotc.core.TythanProvider;
 import co.lotc.core.agnostic.Command;
+import co.lotc.core.agnostic.Sender;
 import co.lotc.core.bungee.command.BrigadierInjector;
 import co.lotc.core.bungee.command.BungeeCommandData;
 import co.lotc.core.bungee.command.BungeeCommandExecutor;
@@ -20,13 +16,28 @@ import de.exceptionflug.protocolize.api.protocol.ProtocolAPI;
 import lombok.Getter;
 import lombok.var;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
 public class TythanBungee extends Plugin implements Tythan {
 	@Getter private boolean debugging;
+	public static final Function<Sender, CommandSender> UNWRAP_SENDER = s->((BungeeSender) s).getHandle();
+	public static final Function<Sender, ProxiedPlayer> UNWRAP_PLAYER = UNWRAP_SENDER.andThen(s->(s instanceof ProxiedPlayer)? ((ProxiedPlayer) s):null);
+	public static final Supplier<List<String>> PLAYER_COMPLETER = ()-> ProxyServer.getInstance().getPlayers().stream().map(ProxiedPlayer::getName).collect(Collectors.toList());
 	
 	@Override
 	public void onLoad() {
@@ -46,7 +57,19 @@ public class TythanBungee extends Plugin implements Tythan {
 	}
 	
 	private void registerCommandParameterTypes() {
-		new ParameterType<>(CommandSender.class).senderMapper(s->((BungeeSender) s).getHandle()).register();
+		new ParameterType<>(CommandSender.class).senderMapper(UNWRAP_SENDER).register();
+		new ParameterType<>(ProxiedPlayer.class)
+				.senderMapper(UNWRAP_PLAYER)
+				.mapperWithSender((send,s)->{
+					if("@p".equals(s)) s = send.getName();
+					if(s.length() == 36) {
+						try {return ProxyServer.getInstance().getPlayer(UUID.fromString(s));}
+						catch(IllegalArgumentException e) {return null;}
+					} else {
+						return ProxyServer.getInstance().getPlayer(s);
+					}
+				})
+				.completer(PLAYER_COMPLETER);
 	}
 	
 	@Override
