5fe60a333a8e37566be2b4fa52ec427e5672147a
Sporadic <pino.bakker@gmail.com>
2019-04-14 16:50:13 +0200

    here's a command completer

--- tythan-common/src/main/java/co/lotc/core/command/CommandCompleter.java
+++ tythan-common/src/main/java/co/lotc/core/command/CommandCompleter.java
@@ -1,9 +1,29 @@
 package co.lotc.core.command;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import co.lotc.core.agnostic.Sender;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class CommandCompleter {
-
-	public CommandCompleter() {
-		// TODO Auto-generated constructor stub
+	public static final BiFunction<Sender, String, ? extends Collection<String>> NULL_COMPLETER = ($1,$2)->Collections.emptyList();
+	
+	private final BiFunction<Sender, String, ? extends Collection<String>> suggestions;
+	
+	public CommandCompleter(Supplier<Collection<String>> supplier) {
+		suggestions = ($1,$2) -> supplier.get();
 	}
-
+	
+	public CommandCompleter(Function<Sender, ? extends Collection<String>> function) {
+		suggestions = (s,$)-> function.apply(s);
+	}
+	
+	public Collection<String> suggest(Sender sender, String input){
+		return suggestions.apply(sender, input);
+	}
 }
