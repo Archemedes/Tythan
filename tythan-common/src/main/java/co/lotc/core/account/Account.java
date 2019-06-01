33d3bf37a9ce2b13b4ecb984027e85888d877552
Sporadic <pino.bakker@gmail.com>
2019-06-01 14:20:56 +0200

    skeleton for the accounts

---
+++ tythan-common/src/main/java/co/lotc/core/account/Account.java
@@ -0,0 +1,32 @@
+package co.lotc.core.account;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter
+public class Account {
+	private final int id=-1;
+	
+	private long forumId; //e.g. 22277 = sporadic
+	private long discordId; //e.g 69173255004487680=Telanir
+	
+	private int fatigue;
+	private long timeCreatedMillis;
+	
+	//private final Tags<Account> tags;
+	
+	transient long timePlayed, timePlayedThisWeek, lastSeen;
+	
+	@Getter(AccessLevel.NONE) transient final Set<UUID> alts = new HashSet<>();
+	@Getter(AccessLevel.NONE) transient final Set<String> ips = new HashSet<>();
+	
+	
+	public Instant getTimeCreated() {
+		return Instant.ofEpochMilli(timeCreatedMillis);
+	}
+}
---
+++ tythan-common/src/main/java/co/lotc/core/account/AccountHandler.java
@@ -0,0 +1,27 @@
+package co.lotc.core.account;
+
+import java.util.function.Consumer;
+
+public class AccountHandler {
+
+	public AccountHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void tryLoadAccount(Consumer<Account> callback) {
+		
+	}
+
+	public void forceLoadAccount(Consumer<Account> callback) {
+		
+	}
+	
+	public void viewAccount(Consumer<Account> callback) {
+		
+	}
+	
+	
+	private Account fetch(int id) {
+		
+	}
+}
